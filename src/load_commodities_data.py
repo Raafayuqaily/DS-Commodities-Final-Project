2a1ab383320a9ea24814123e0f24b420300b29da
Kaleem Bukhari <kaleembukhari90@gmail.com>
2024-03-06 23:27:59 -0600

    Function to load Commodities CSV
    The function looks into the directory and loads 'commodities.csv' file.
    Note: This is a manual pull so the directory name reflects that.

---
+++ src/load_commodities_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import config
+from pathlib import Path
+
+DATA_DIR = Path(config.DATA_DIR)
+
+def load_data(data_dir=DATA_DIR, file_name = "commodities_data.csv"):
+    """
+    Load commodities data csv, stored in /data/manual
+    Setting the index of the dataframe to Date
+    Returning the dataframe
+    """
+    file_path = Path(data_dir) / "manual" / file_name
+    df = pd.read_csv(file_path)
+    df.set_index('Date', inplace=True)
+    return df
+
+def demo():
+    """
+    Creating an execution function for load_data
+    that prints the first 5 rows of the dataframe
+    """
+    df = load_data()
+    print(df.head())
+
+if __name__ == "_main_":
+    demo()
+
+
+
