01dd8b61fe741f773e2a74538dbd22560fd7594b
Kaleem Bukhari <kaleembukhari90@gmail.com>
2024-03-07 02:35:55 -0600

    Removed redundancies that don't apply to us

--- src/config.py
+++ src/config.py
@@ -25,14 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_DIR = (BASE_DIR / config('DATA_DIR', default=Path('data'), cast=Path)).resolve()
 OUTPUT_DIR = (BASE_DIR / config('OUTPUT_DIR', default=Path('output'), cast=Path)).resolve()
-WRDS_USERNAME = config("WRDS_USERNAME", default="")
-START_DATE = config("START_DATE", default="1913-01-01", cast=pd.to_datetime)
-END_DATE = config("END_DATE", default="2023-10-01", cast=pd.to_datetime)
 
 if __name__ == "__main__":
     
     ## If they don't exist, create the data and output directories
-    (DATA_DIR / 'pulled').mkdir(parents=True, exist_ok=True)
+    # (DATA_DIR / 'manual').mkdir(parents=True, exist_ok=True)
 
     # Sometimes, I'll create other folders to organize the data
     # (DATA_DIR / 'intermediate').mkdir(parents=True, exist_ok=True)
