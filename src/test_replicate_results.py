851175a091feb8093f10215629aadd1403822552
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-08 06:33:00 -0500

    Update test_replicate_results.py

--- src/test_replicate_results.py
+++ src/test_replicate_results.py
@@ -9,27 +9,6 @@ import replicate_results
 
 DATA_DIR = Path(config.DATA_DIR)
 
-def test_compute_num_observations():
-
-    df = load_commodities_data.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
-    prep_df = data_preprocessing.preprocess_data(df)
-
-    # Test if the function computes the number of observations correctly
-    # obs_df = replicate_results.compute_num_observations(prep_df)
-    # Test if the function returns a pandas DataFrame
-    # assert isinstance(obs_df, pd.DataFrame)
-
-    # Test if the DataFrame is not empty
-    # assert not obs_df.empty
-
-    # Test if the function computes the number of observations correctly
-    obs_series = replicate_results.compute_num_observations(prep_df)['N']  # Assuming 'N' is the column containing the number of observations
-    # Test if the result is a pandas Series
-    assert isinstance(obs_series, pd.Series)
-
-    # Test if the Series is not empty
-    assert not obs_series.empty
-
 
 def test_compute_performance_metrics():
 
@@ -40,27 +19,6 @@ def test_compute_performance_metrics():
     excess_returns_df = replicate_results.compute_commodity_excess_returns(prep_df)
     performance_metrics = replicate_results.compute_performance_metrics(excess_returns_df)
     assert performance_metrics.dtypes.apply(pd.api.types.is_numeric_dtype).all()
-
-def test_compute_basis_mean():
-
-    df = load_commodities_data.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
-    prep_df = data_preprocessing.preprocess_data(df)
-
-    # Test if the function computes basis mean with numerical values
-    # basis_mean = replicate_results.compute_basis_mean(prep_df)
-    # Test if the function returns a pandas DataFrame
-    # assert isinstance(basis_mean, pd.DataFrame)
-
-    # Test if the DataFrame is not empty
-    # assert not basis_mean.empty
-
-    # Test if the function computes basis mean with numerical values
-    basis_mean_series = replicate_results.compute_basis_mean(prep_df)['Basis']  # Assuming 'Basis' is the column containing the basis mean
-    # Test if the result is a pandas Series
-    assert isinstance(basis_mean_series, pd.Series)
-
-    # Test if the Series is not empty
-    assert not basis_mean_series.empty
 
 
 def test_compute_freq_backwardation():
