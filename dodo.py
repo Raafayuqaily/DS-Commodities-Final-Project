47570bb3f465187d957a311fd80b126c55e4564f
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-08 06:36:03 -0500

    Remove extra stuff

--- dodo.py
+++ dodo.py
@@ -97,44 +97,6 @@ def task_data_preprocessing():
     }
 
 
-
-# def task_pull_data_via_presto():
-#     """
-#     Run several data pulls
-
-#     This will run commands like this:
-#     presto-cli --output-format=CSV_HEADER --file=/data/unixhome/src/sql_gross_performance.sql > /data/unixhome/src/sometest.csv
-
-#     """
-#     sql_pulls_dict = {
-#         'sometest.sql':'sometest.csv',
-#     }
-#     file_dep = list(sql_pulls_dict.keys())
-#     file_output = list(sql_pulls_dict.values())
-
-#     targets = [PRIVATE_DATA_DIR / 'sql_pulled' / file for file in file_output]
-
-#     def action_string(sql_file, csv_output):
-#         s = f"""
-#             ssh sql.someurl.com <<-'ENDSSH'
-#             echo Starting Presto Pull Command for {sql_file}
-#             cd {getcwd()}
-#             presto-cli --output-format=CSV_HEADER --file={sql_file} > {csv_output}
-#             """
-#         return s
-#     actions = [
-#                 action_string(sql_file,
-#                               (PRIVATE_DATA_DIR / 'sql_pulled' / sql_pulls_dict[sql_file])
-#                               ) for sql_file in sql_pulls_dict
-#             ]
-#     return {
-#         "actions":actions,
-#         "targets": targets,
-#         'task_dep':[],
-#         "file_dep": file_dep,
-#     }
-
-
 def task_summary_stats():
     """ """
     file_dep = ["./src/example_table.py"]
@@ -166,36 +128,6 @@ def task_example_plot():
             "ipython ./src/example_plot.py",
         ],
         "targets": targets,
-        "file_dep": file_dep,
-        "clean": True,
-    }
-
-
-def task_convert_notebooks_to_scripts():
-    """Preps the notebooks for presentation format.
-    Execute notebooks with summary stats and plots and remove metadata.
-    """
-    build_dir = Path(OUTPUT_DIR)
-    build_dir.mkdir(parents=True, exist_ok=True)
-
-    notebooks = [
-        "01_example_notebook.ipynb",
-        "02_interactive_plot_example.ipynb",
-    ]
-    file_dep = [Path("./src") / file for file in notebooks]
-    stems = [notebook.split(".")[0] for notebook in notebooks]
-    targets = [build_dir / f"_{stem}.py" for stem in stems]
-
-    actions = [
-        # *[jupyter_execute_notebook(notebook) for notebook in notebooks_to_run],
-        # *[jupyter_to_html(notebook) for notebook in notebooks_to_run],
-        *[jupyter_clear_output(notebook) for notebook in stems],
-        *[jupyter_to_python(notebook, build_dir) for notebook in stems],
-    ]
-    return {
-        "actions": actions,
-        "targets": targets,
-        "task_dep": [],
         "file_dep": file_dep,
         "clean": True,
     }
@@ -290,35 +222,3 @@ def task_compile_sphinx_docs():
         "task_dep": ["run_notebooks"],
         "clean": True,
     }
-
-
-# def task_knit_RMarkdown_files():
-#     """Preps the RMarkdown files for presentation format.
-#     This will knit the RMarkdown files for easier sharing of results.
-#     """
-#     files_to_knit = [
-#         'shift_share.Rmd',
-#         ]
-
-#     files_to_knit_stems = [file.split('.')[0] for file in files_to_knit]
-
-#     file_dep = [
-#         'load_performance_and_loan_merged.py',
-#         *[file + ".Rmd" for file in files_to_knit_stems],
-#         ]
-
-#     file_output = [file + '.html' for file in files_to_knit_stems]
-#     targets = [OUTPUT_DIR / file for file in file_output]
-
-#     def knit_string(file):
-#         return f"""Rscript -e 'library(rmarkdown); rmarkdown::render("{file}.Rmd", output_format="html_document", OUTPUT_DIR="../output/")'"""
-#     actions = [knit_string(file) for file in files_to_knit_stems]
-#     return {
-#         "actions": [
-#                     "module use -a /opt/aws_opt/Modulefiles",
-#                     "module load R/4.2.2",
-#                     *actions],
-#         "targets": targets,
-#         'task_dep':[],
-#         "file_dep": file_dep,
-#     }
