e6ae43d5adec6693ef81a73714b915e2b415e14d
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-08 06:23:27 -0500

    Update test_perform_additional_analysis.py

--- src/test_perform_additional_analysis.py
+++ src/test_perform_additional_analysis.py
@@ -13,84 +13,73 @@ OUTPUT_DIR = Path(config.OUTPUT_DIR)
 
 
 def test_plot_commodities_by_sector():
-
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
     paa.plot_commodities_by_sector(df, output_dir)
-    assert os.path.exists(output_dir.join("commodities_by_sector.png"))
+    assert (output_dir / "commodities_by_sector.png").is_file()
 
 
 def test_plot_data_availability():
-    
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
     paa.plot_data_availability(df, output_dir)
-    assert os.path.exists(output_dir.join("data_availability_heatmap.png"))
+    assert (output_dir / "data_availability_heatmap.png").is_file()
 
 
 def test_plot_max_contract_number():
-
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
     paa.plot_max_contract_number(df, output_dir)
-    assert os.path.exists(output_dir.join("maximum_contract_number.png"))
+    assert (output_dir / "maximum_contract_number.png").is_file()
 
 
 def test_plot_max_contract_availability():
-
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
     paa.plot_max_contract_availability(df, output_dir)
-    assert os.path.exists(output_dir.join("max_contract_availability.png"))
+    assert (output_dir / "max_contract_availability.png").is_file()
 
 
-def test_plot_commodity_time_series(df):
-
+def test_plot_commodity_time_series():
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
-    paa.plot_commodity_time_series(df, output_dir, commodity='A')
-    assert os.path.exists(output_dir.join("A_futures_contracts_time_series.png"))
+    commodity = 'A'  # You need to specify the commodity for this test
+    paa.plot_commodity_time_series(df, output_dir, commodity=commodity)
+    assert (output_dir / f"{commodity}_futures_contracts_time_series.png").is_file()
 
 
 def test_plot_return_distribution_and_save_formatted_stats():
-
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
-    paa.plot_return_distribution_and_save_formatted_stats(df, output_dir, contract_num=1)
-    assert os.path.exists(output_dir.join("monthly_returns_distribution_contract_1.png"))
-    assert os.path.exists(output_dir.join("monthly_returns_stats_contract_1.tex"))
+    contract_num = 1  # You need to specify the contract number for this test
+    paa.plot_return_distribution_and_save_formatted_stats(df, output_dir, contract_num=contract_num)
+    assert (output_dir / f"monthly_returns_distribution_contract_{contract_num}.png").is_file()
+    assert (output_dir / f"monthly_returns_stats_contract_{contract_num}.tex").is_file()
 
 
 def test_plot_rolling_volatility():
-
     df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
     df = dp.preprocess_data(df)
     output_dir = Path(config.OUTPUT_DIR)
 
-    paa.plot_rolling_volatility(df, output_dir, rolling_window=60, contract_num=1)
-    assert os.path.exists(output_dir.join("60_months_rolling_volatility.png"))
+    rolling_window = 60  # You need to specify the rolling window for this test
+    contract_num = 1  # You need to specify the contract number for this test
+    paa.plot_rolling_volatility(df, output_dir, rolling_window=rolling_window, contract_num=contract_num)
+    assert (output_dir / f"{rolling_window}_months_rolling_volatility.png").is_file()
 
-
-def test_plot_rolling_sharpe_ratio():
-
-    df = lcd.load_data(data_dir=DATA_DIR, file_name="commodities_data_2024.csv")
-    df = dp.preprocess_data(df)
-    output_dir = Path(config.OUTPUT_DIR)
-
-    paa.plot_rolling_sharpe_ratio(df, output_dir, rolling_window=60, contract_num=1)
-    assert os.path.exists(output_dir.join("60_months_rolling_sharpe_ratio.png"))
 
 if __name__ == '__main__':
     pytest.main()
+
