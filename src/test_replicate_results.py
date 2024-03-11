53dcdfbb99ee490ef15a71648b828bffdf51b3c6
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-10 21:10:05 -0400

    Update test_replicate_results.py

--- src/test_replicate_results.py
+++ src/test_replicate_results.py
@@ -1,3 +1,9 @@
+"""
+This module tests the performance metrics and backwardation frequency computation 
+from the replicate_results module. It verifies that the returned values are numeric 
+and that the backwardation frequency is non-negative.
+"""
+
 import pandas as pd
 import pytest
 import config
@@ -13,6 +19,11 @@ end_ = config.ENDDATE_OLD[:4]
 
 
 def test_compute_performance_metrics():
+    """
+    Tests the computation of performance metrics for commodities.
+    Ensures that all performance metrics computed are of numerical data types.
+    """
+
     clean_data_file_path = Path(DATA_DIR) / "manual"/f"clean_{start_}_{end_}_{INPUTFILE}"
     clean_data_df = pd.read_csv(clean_data_file_path)
 
@@ -23,12 +34,35 @@ def test_compute_performance_metrics():
 
 
 def test_compute_freq_backwardation():
+    """
+    Tests the computation of frequency of backwardation for commodities.
+    Ensures that the computed frequency of backwardation is non-negative.
+    """
+
     clean_data_file_path = Path(DATA_DIR) / "manual"/f"clean_{start_}_{end_}_{INPUTFILE}"
     clean_data_df = pd.read_csv(clean_data_file_path)
 
     # Test if the function computes frequency of backwardation with positive numerical values
     freq_backwardation = replicate_results.compute_freq_backwardation(clean_data_df)
     assert (freq_backwardation['Freq. of Backwardation'] >= 0).all()
+
+def test_sharpe_ratio_range():
+    """
+    Tests if the computed Sharpe Ratios are within the expected range between -100 and +100.
+    This is to ensure that the Sharpe Ratio values are within a reasonable range and correctly computed.
+    """
+
+    clean_data_file_path = Path(DATA_DIR) / f"manual/clean_{start_}_{end_}_{INPUTFILE}"
+    clean_data_df = pd.read_csv(clean_data_file_path)
+
+    # Compute excess returns for the clean data
+    excess_returns_df = replicate_results.compute_commodity_excess_returns(clean_data_df)
+
+    # Compute performance metrics which include the Sharpe Ratio
+    performance_metrics = replicate_results.compute_performance_metrics(excess_returns_df)
+
+    # Assert that all Sharpe Ratio values are between -100 and +100
+    assert all(-100 <= value <= 100 for value in performance_metrics['Ann. Sharpe Ratio']), "Sharpe Ratio values are out of the expected range (-100 to +100)."
 
 if __name__ == '__main__':
     pytest.main()
