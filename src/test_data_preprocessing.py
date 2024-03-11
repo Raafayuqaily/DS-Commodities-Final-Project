cefd2e8591c38c1497e097dd5d4ab873f0433b3c
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-10 20:07:52 -0400

    Update test_data_preprocessing.py

--- src/test_data_preprocessing.py
+++ src/test_data_preprocessing.py
@@ -1,3 +1,9 @@
+"""
+This module is designed to test the data preprocessing functionalities provided by the 
+data_preprocessing module. It checks if the data types, sorting, and column names of 
+the processed data match expected outcomes.
+"""
+
 import pandas as pd
 import pytest
 import config
@@ -11,7 +17,12 @@ STARTDATE = config.STARTDATE_OLD
 ENDDATE = config.ENDDATE_OLD
 
 def test_preprocess_data_types():
-    
+    """
+    Tests the types of the columns in the processed data to ensure they match expected types.
+    Specifically, it checks that the 'ClosePrice' column is of type float, the 'Contract' column 
+    is of type int, and the DataFrame index is of datetime type.
+    """
+
     processed_data = data_preprocessing.clean_process_data(start_date = STARTDATE, end_date = ENDDATE, data_dir = DATA_DIR, input_file = INPUTFILE)
 
     # Test that data types are as expected after preprocessing
@@ -20,14 +31,21 @@ def test_preprocess_data_types():
     assert processed_data.index.dtype == '<M8[ns]'
 
 def test_preprocess_data_sorting():
-    
+    """
+    Tests that the processed data is sorted correctly.
+    It verifies that the DataFrame index (which should be dates) is sorted in ascending order.
+    """
+
     processed_data = data_preprocessing.clean_process_data(start_date = STARTDATE, end_date = ENDDATE, data_dir = DATA_DIR, input_file = INPUTFILE)
 
     # Test that the data is sorted correctly after preprocessing
     assert (processed_data.index == processed_data.index.sort_values()).all()
 
 def test_preprocess_data_column_names():
-    
+    """
+    Tests that the processed data contains all expected columns after preprocessing.
+    It checks for the presence of 'Commodity', 'Contract', 'ClosePrice', and 'YearMonth' columns.
+    """
     processed_data = data_preprocessing.clean_process_data(start_date = STARTDATE, end_date = ENDDATE, data_dir = DATA_DIR, input_file = INPUTFILE)
 
     # Test that column names are as expected after preprocessing
