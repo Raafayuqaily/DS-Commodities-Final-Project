e32c42e75e2a80a5bd6defd62f08a07fcda83b7a
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-03-11 17:34:13 -0400

    doc-string add.

--- src/latex_to_document.py
+++ src/latex_to_document.py
@@ -1,11 +1,19 @@
-#import subprocess
+"""
+This script compiles a LaTeX file into a PDF document using the pdflatex engine. It takes the path to a LaTeX file as an input argument, converts it to PDF, and saves the output in the same location with the same name but with a .pdf extension. If the compilation fails, it provides error messages and exits. The script is designed to be used as a standalone tool or integrated into a larger LaTeX project workflow.
+"""
+
 from pathlib import Path
 import subprocess
 import sys
 
-latex_path = "reports/report_simple_example_copy.tex"
+latex_path = "reports/Final_Report.tex"
 
 def latex_to_document(latex_file):
+    """
+    This function uses the 'pdflatex' command to compile a LaTeX file specified by 'latex_file' into a PDF. 
+    It checks for compilation success and informs the user of the outcome. If there are compilation errors, 
+    it provides a message to check the log file for details.
+    """
     output_pdf = Path(latex_file).with_suffix('.pdf')
     try:
         # Attempt to compile the LaTeX file into a PDF
